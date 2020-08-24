ae32ebdbeab642cee9da6b4c49964e991f6b2b51
Mykhailo Pechkurov <mykhailo.pechkurov@here.com>
2020-08-24 22:32:47 +0200

    Initial commit

---
+++ src/main/java/com/example/university/UniversityMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityMongoApplication.class, args);
+	}
+
+}
